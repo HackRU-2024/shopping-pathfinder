76d55ad75b40c39f3cc3abdcb6327a686281d2ea
Evelyn914 <ehernandezj914@gmail.com>
2024-03-24 02:52:47 -0400

    improved layouts

--- views/mainWindow.py
+++ views/mainWindow.py
@@ -1,72 +1,83 @@
 import sys
-from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, 
+from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QLabel,
                              QListWidgetItem, QStackedWidget, QLineEdit, QListWidget, QHBoxLayout)
+from PyQt6 import QtCore
+#from tilemap import TileMap
+#list to store products
+product_list = []
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('QStackedWidget Demo')
+        self.setWindowTitle('Demo')
         
         # Create a stacked widget
         self.stacked_widget = QStackedWidget()
 
-        #(1st page) Create pages (widgets) to add to the stacked widget
-        self.page1 = QWidget()
+        #(1st page) Create pages (widgets) to add to the stacked widget----------
+        self.main_page = QWidget()
         # Add labels to each page
-        label1 = QLabel('italian sandwich')
-        layout1 = QVBoxLayout(self.page1)
-        layout1.addWidget(label1)
+        header = QLabel('Main Menu')
+        header.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        #header.setMaximumSize(QtCore.QSize(16777215, 20))
+        goto_additem_view_btn = QPushButton('Search Product')
+        goto_additem_view_btn.clicked.connect(self.show_addItem_page)
+        main_layout = QVBoxLayout(self.main_page)
+        main_layout.addWidget(header)
+        main_layout.addWidget(goto_additem_view_btn)
 
-        #second page (widget)
-        self.page2 = QWidget()
-        #add label
-        label2 = QLabel('taco')
+        #search product page (widget) ----------
+        self.search_product_page = QWidget()
+
         # line field for user input
         self.user_input_line = QLineEdit()
         self.user_input_line.setMaxLength(10)
         self.user_input_line.setPlaceholderText("Search product")
 
         #btn to submit str product
-        self.submit_btn = QPushButton('Submit')
-        self.submit_btn.clicked.connect(self.add_product_to_list)
+        self.add_product_btn = QPushButton('Add')
+        self.add_product_btn.clicked.connect(self.add_product_to_list)
 
         #horizontal layout for user input str function
         user_input_Hbox = QHBoxLayout()
         user_input_Hbox.addWidget(self.user_input_line)
-        user_input_Hbox.addWidget(self.submit_btn)
-
-        #list to store products
-        self.product_list = []
+        user_input_Hbox.addWidget(self.add_product_btn)
 
         #list of input data
         self.product_list_widget = QListWidget()
-        #self.product_list_widget.addItems(["One", "Two", "Three"])
+        self.product_list_widget.setAlternatingRowColors(True)
 
-        layout2 = QVBoxLayout(self.page2)
-        layout2.addWidget(label2)
-        layout2.addLayout(user_input_Hbox)
-        layout2.addWidget(self.product_list_widget)
+        #button to switch to tile map
+        self.display_tilemap_btn = QPushButton('Map')
+        #self.display_tilemap_btn.clicked.connect(self.show_tilemap_page)
 
-        # Add pages (qwidgets) to the stacked widget
-        self.stacked_widget.addWidget(self.page1)
-        self.stacked_widget.addWidget(self.page2)
+        # Create buttons to switch to main menu
+        self.main_menu_btn = QPushButton('Main Menu')
+        self.main_menu_btn.clicked.connect(self.show_mainMenu_page)
 
-        # Create buttons to switch between pages
-        self.button_page1 = QPushButton('first page')
-        self.button_page1.clicked.connect(self.show_page1)
-        self.button_page2 = QPushButton('second page')
-        self.button_page2.clicked.connect(self.show_page2)
+        search_product_layout = QVBoxLayout(self.search_product_page)
+        search_product_layout.addLayout(user_input_Hbox)
+        search_product_layout.addWidget(self.product_list_widget)
+        search_product_layout.addWidget(self.display_tilemap_btn)
+        search_product_layout.addWidget(self.main_menu_btn)
+
+
+        #initalizing the tilemap view
+        #self.tilemap_page = TileMap()
+
+        # Add pages (qwidgets) to the stacked widget----------- stacking widgets
+        self.stacked_widget.addWidget(self.main_page)
+        self.stacked_widget.addWidget(self.search_product_page)
+        #self.stacked_widget.addWidget(self.tilemap_page)
 
         # Layout for the main window
         layout = QVBoxLayout(self)
         layout.addWidget(self.stacked_widget)
-        layout.addWidget(self.button_page1)
-        layout.addWidget(self.button_page2)
 
-    def show_page1(self):
+    def show_mainMenu_page(self):
         self.stacked_widget.setCurrentIndex(0)
 
-    def show_page2(self):
+    def show_addItem_page(self):
         self.stacked_widget.setCurrentIndex(1)
     
     #adding to our list & displaying to user
@@ -74,13 +85,17 @@ class MainWindow(QWidget):
         #get the str of product & save
         str = self.user_input_line.text()
         if str:
-            self.product_list.append(str)
+            #if valid string
+            product_list.append(str)
             #show in the list widget display
             item = QListWidgetItem(str)
             self.product_list_widget.addItem(item)
             self.user_input_line.clear() #clear
             #check
             #print(self.product_list)
+    
+    def show_tilemap_page(self):
+        self.stacked_widget.setCurrentIndex(2)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
